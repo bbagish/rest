3e8552a16affc5dd63e168c6811f1191f6abbfea
bbagish <32007864+bbagish@users.noreply.github.com>
2019-09-03 11:44:06 -0400

    Update basics.java

--- RestAPIAutomation/src/test/java/rest/webapi/udemy/basics.java
+++ RestAPIAutomation/src/test/java/rest/webapi/udemy/basics.java
@@ -13,7 +13,7 @@ public class basics {
 		baseURI = "https://maps.googleapis.com/";
 		//equalTo only applies to body
 		given().param("location", "-33.8670522,151.1957362").param("radius", "500")
-				.param("key", "AIzaSyBEcPtgPUKObLbUaiWTm4zFuJfaIUGBp10").when().get("maps/api/place/nearbysearch/json")
+				.param("key", "token").when().get("maps/api/place/nearbysearch/json")
 				.then().assertThat().statusCode(200).and().contentType(ContentType.JSON).and()
 				.body("results[0].name", equalTo("Sydney")).and()
 				.body("results[0].place_id", equalTo("ChIJP3Sa8ziYEmsRUKgyFmh9AQM")) 
